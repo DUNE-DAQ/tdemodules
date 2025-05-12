bf82fca66c6c32fad5266e7f8cf46a68cae53529
ShyamB97 <bhuller.shyam@gmail.com>
2025-05-12 15:33:43 +0200

    rework logic for testing with multiple AMCs

--- test/scripts/test_amc_commands.py
+++ test/scripts/test_amc_commands.py
@@ -10,24 +10,31 @@ import time
 def main():
     
     crate_id = 0
-    slot_id = 1
+    slot_id = [1, 6, 7]
 
-    amc_ctrl_ip = f"10.73.{crate_id+32}.{slot_id+1}"
-    amc_data_port = 54321+(slot_id+1)
-    c = tdemodules.AMCController(amc_ctrl_ip, amc_data_port)
-    print(f"Reading status of card {amc_ctrl_ip} (crate {crate_id}, slot {slot_id})")
-    c.card_status()
-
-    print(f"starting card {slot_id}")
-    c.card_start()
-    print("Waiting")
-    for i in range(5):
-        time.sleep(10)
-        print(f"-- {i*10} s elapsed")
+    controllers = []
+    for s in slot_id:
+        amc_ctrl_ip = f"10.73.{crate_id+32}.{s+1}"
+        amc_data_port = 54321+(s+1)
+        c = tdemodules.AMCController(amc_ctrl_ip, amc_data_port)
+        print(f"Reading status of card {amc_ctrl_ip} (crate {crate_id}, slot {s})")
         c.card_status()
+        controllers.append(c)
 
-    print(f"stopping card {slot_id}")
-    c.card_stop()
+    for s, c in zip(slot_id, controllers):
+        print(f"starting card {s}")
+        c.card_start()
+
+    print("Waiting")
+    t_interval = 10
+    for i in range(5):
+        time.sleep(t_interval)
+        print(f"-- {i*t_interval} s elapsed")
+        # c.card_status()
+
+    for s, c in zip(slot_id, controllers):
+        print(f"stopping card {s}")
+        c.card_stop()
 
 
 
